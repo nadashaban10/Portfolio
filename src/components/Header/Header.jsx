9971977a277c4bc0787c42efa60fad4e7e731695
nadashaban10 <nadashaban10@github.com>
2024-12-13 12:56:49 +0200

    portfolio project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,10 +34,9 @@ function Header() {
           <div className="fixed">
             <ul className="modal">
               <li className="close-icon"><a className="icon-button-close" onClick={() => setShowMenu(false)}><FaTimes /></a></li> 
-              <li><a href="#">About</a></li>
-              <li><a href="#">Projects</a></li>
-              <li><a href="#">Portfolio</a></li>
-              <li><a href="#">Contact</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
         )}
